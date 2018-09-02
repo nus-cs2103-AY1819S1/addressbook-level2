f1888b3d090d5510095fdde71943a0e11bd4c2e1
Kai Ting <kaiting_95@hotmail.com>
2018-09-02 15:18:54 +0800

    Main.java: add handling for RuntimeException for read-only files

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import java.lang.RuntimeException;
+
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.commands.ExitCommand;
@@ -80,15 +82,21 @@ public class Main {
 
     /** Reads the user command and executes it, until the user issues the exit command.  */
     private void runCommandLoopUntilExitCommand() {
-        Command command;
-        do {
-            String userCommandText = ui.getUserCommand();
-            command = new Parser().parseCommand(userCommandText);
-            CommandResult result = executeCommand(command);
-            recordResult(result);
-            ui.showResultToUser(result);
+        try {
+            Command command;
+            do {
+                String userCommandText = ui.getUserCommand();
+                command = new Parser().parseCommand(userCommandText);
+                CommandResult result = executeCommand(command);
+                recordResult(result);
+                ui.showResultToUser(result);
 
-        } while (!ExitCommand.isExit(command));
+            } while (!ExitCommand.isExit(command));
+        }
+        catch (RuntimeException e) {
+            System.out.println("File is read-only.");
+            exit();
+        }
     }
 
     /** Updates the {@link #lastShownList} if the result contains a list of Persons. */
