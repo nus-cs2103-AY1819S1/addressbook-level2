0d73795a5810070c736ffdd8d7f5cbd1ecd3b3e3
Jack Wong <jackwong3101@yahoo.com>
2018-09-02 14:16:02 +0800

    Add test for sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -8,4 +8,8 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the list of people in the address book. ";
+
+    public SortCommand() {
+
+    }
 }
