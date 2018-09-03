4cebaa8f096eee4c8b40e797f082fd718575ad3f
DESKTOP-4O94ROD\Andy <andychan1451@gmail.com>
2018-09-03 21:39:00 +0800

    Add Test Cases for Utils.isAnyNull Method

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,12 +1,11 @@
 package seedu.addressbook.common;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UtilsTest {
 
@@ -42,5 +41,32 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    private void assertIsNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertIsNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
+    @Test
+    public void isAnyNull() throws Exception {
+        // Empty
+        assertIsNotNull();
+
+        // One String
+        assertIsNotNull("abc");
+
+        // Two Strings
+        assertIsNotNull("abc", "def");
+
+        // No string
+        assertIsNull((Object) null);
+
+        // One string
+        assertIsNull("abc", null);
+        assertIsNull(null, "abc");
     }
 }
