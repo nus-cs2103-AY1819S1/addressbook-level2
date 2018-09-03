940f800fa6d156a5649382584cc3ee7be6681376
Nyxf4ll_ <vosk1093@gmail.com>
2018-09-03 18:03:08 +0800

    Add Sort Command
    Sort command is not present in current version of the software.
    
    Since sorting makes the list easier to read especially when the number people in the address book is high.
    
    So let's add a sort command.

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -10,7 +10,7 @@ import seedu.addressbook.data.tag.Tag;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Person implements ReadOnlyPerson, Comparable<Person>  {
 
     private Name name;
     private Phone phone;
@@ -81,6 +81,11 @@ public class Person implements ReadOnlyPerson {
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
         return Objects.hash(name, phone, email, address, tags);
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        return this.name.compareTo(p.name);
     }
 
     @Override
