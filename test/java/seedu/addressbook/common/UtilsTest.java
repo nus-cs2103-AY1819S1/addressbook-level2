2bf52cf32210cd546a8bda2b3e50ebe8ed7542f9
jhengy <35480886+jhengy@users.noreply.github.com>
2018-09-05 23:13:40 +0800

    Added Test to Utils.isAnyNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,16 +1,15 @@
 package seedu.addressbook.common;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Test;
-
 public class UtilsTest {
 
-
+    //this is a method of UtilsTest that tests the method Utils.elementsAreUnique
     @Test
     public void elementsAreUnique() throws Exception {
         // empty list
@@ -43,4 +42,41 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+
+
+    // this is a method for testing the method Utils.isAnyNull
+    @Test 
+    public void isAnyNullTest() {
+        // empty list --> why does it lead to test failure
+        //assertIsAnyNull();
+        //assertAllNotNull();
+
+        // only one object
+        assertAllNotNull("");
+        assertAllNotNull(Integer.valueOf("2"));
+        assertAllNotNull(1);
+        assertIsAnyNull((Object) null);
+
+        // three objects, containing at least 1 null object, at different positions
+        assertIsAnyNull((Object) null, "j", "p");
+        assertIsAnyNull("j", (Object) null, "p");
+        assertIsAnyNull("j", "p", (Object) null);
+        assertIsAnyNull("j", (Object) null, (Object) null);
+        assertIsAnyNull((Object) null, (Object) null, (Object) null);
+
+
+        // three none null objects
+        assertAllNotNull(1, "kjsa", "");
+
+
+    }
+
+
+    private void assertIsAnyNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertAllNotNull (Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    } 
 }
