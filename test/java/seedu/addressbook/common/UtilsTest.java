f3111c82047b6b66440428689aa67848d48babd5
NELVINPC\HP <nelvintan@gmail.com>
2018-09-05 18:39:31 +0800

    edit the test cases for isAnyNull() method in UtilsTest.java

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -46,10 +46,25 @@ public class UtilsTest {
 
     @Test
     public void isAnyNull() throws Exception {
+
+        // Objects to use for testing
         Object object = new Object();
         String string = "Test";
         int integer = 1;
+
+        // empty list test
+        assertFalse(Utils.isAnyNull());
+
+        // non empty list test
         assertFalse(Utils.isAnyNull(object, string, integer));
+
+        // non empty list with null at front
+        assertTrue(Utils.isAnyNull(null, string, integer));
+
+        // non empty list with null in the middle
+        assertTrue(Utils.isAnyNull(object, null, string, integer));
+
+        // non empty list with null at the end
         assertTrue(Utils.isAnyNull(object, string, integer, null));
     }
 
