f48318a9d1008ac584bb5b28c2c14cc794f238b3
Zulqarnain <zul030995@gmail.com>
2018-09-05 15:55:58 +0800

    Added unit tests for the Utils#isAnyNull(Object…​) method in UtilTest.java

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -15,16 +15,19 @@ public class UtilsTest {
     public void elementsAreUnique() throws Exception {
         // empty list
         assertAreUnique();
+        assertIsAnyNull();
 
         // only one object
         assertAreUnique((Object) null);
         assertAreUnique(1);
         assertAreUnique("");
         assertAreUnique("abc");
+        assertIsAnyNull(1);
 
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
+        assertIsAnyNull(1,2);
 
         // some identical objects
         assertNotUnique("abc", "abc");
@@ -34,7 +37,9 @@ public class UtilsTest {
         assertNotUnique(null, 1, Integer.valueOf(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+        assertIsAnyNull(null, 1, "a", null);
     }
+
 
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
@@ -42,5 +47,9 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    private void assertIsAnyNull(Object... objects) {
+      assertFalse(Utils.isAnyNull(Arrays.asList(objects)));
     }
 }
