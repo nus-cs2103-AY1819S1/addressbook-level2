fd5069eb2d9e2cd9bbd9097befa726cdca8e6284
circularhorses <jefferykwoh@gmail.com>
2018-09-07 22:43:29 +0800

    Add test cases for Utils.isAnyNull
    - empty list
    - single objects
    - multiple objects
    - null (fails when not typecasted to object)

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -42,5 +41,48 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    @Test
+    /**
+     *
+     */
+    public void elementsAreNull() throws Exception {
+        // empty list
+        assertNoNull();
+
+        // single object no null
+        assertNoNull("");
+        assertNoNull("abc");
+        assertNoNull(1);
+        assertNoNull(true);
+        assertNoNull(false);
+
+        // all objects without null
+        assertNoNull("abc", "ab", "a");
+        assertNoNull(1, 2);
+        assertNoNull("abc", "abc");
+        assertNoNull("abc", "", "abc", "ABC");
+        assertNoNull("", "abc", "a", "abc");
+        assertNoNull(1, Integer.valueOf(1));
+
+        // some objects with null
+        assertContainsNull(null, 1, Integer.valueOf(1));
+        assertContainsNull(null, null);
+        assertContainsNull(null, "a", "b", null);
+
+        // null with Object typecast
+        assertContainsNull((Object) null);
+
+        // null without typecast
+        assertContainsNull(null);
+    }
+
+    private void assertContainsNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertNoNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
     }
 }
