de56ae2b57a6fc4d61da1161cbde802003202206
alistair <ongalistair@gmail.com>
2018-09-07 10:38:21 +0800

    Add test for isAnyNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,12 +3,24 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
 public class UtilsTest {
+
+    @Test
+    public void isAnyNull() throws Exception {
+        assertFalse(Utils.isAnyNull(""));
+        assertFalse(Utils.isAnyNull(0));
+        assertFalse(Utils.isAnyNull(new ArrayList<>()));
+        assertFalse(Utils.isAnyNull(new NullPointerException()));
+
+        assertTrue(Utils.isAnyNull(1,2,3,null));
+        assertTrue(Utils.isAnyNull(null, null));
+        assertTrue(Utils.isAnyNull(null)); //test fails at this portion
+    }
 
 
     @Test
