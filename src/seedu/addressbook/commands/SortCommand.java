d6b69f9a74409fde03790247717a7bdb7996fdec
souless94 <cl.wk@hotmail.com>
2018-09-08 14:43:08 +0800

    add sort function description to sort addressbook in ascending order

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -9,7 +9,7 @@ public class SortCommand extends Command {
   public static final String COMMAND_WORD = "sort";
 
   public static final String MESSAGE_USAGE = COMMAND_WORD
-      + ": Sorts all persons in the address book by name.\n"
+      + ": Sorts all persons in the address book by name in ascending order.\n"
       + "Example: " + COMMAND_WORD;
 
   @Override
