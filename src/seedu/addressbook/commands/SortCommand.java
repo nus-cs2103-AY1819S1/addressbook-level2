6c26badf05e91b0c7ac6d9e1d51155db9ffa4ec7
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2018-09-08 21:32:45 +0800

    add sort command in the help menu

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,12 +6,13 @@ import java.util.Comparator;
 import java.util.List;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
-public class SortCommand extends Command{
+public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Displays all persons in the address book as a list, according to their names in"
+            + " alphabetical order, with index numbers.\n"
             + "Example: " + COMMAND_WORD;
 
 
