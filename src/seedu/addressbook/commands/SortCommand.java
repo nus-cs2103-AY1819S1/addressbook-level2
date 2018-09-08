3b94981ad352961ee1951da4088c4341b52d3f90
Kok Yuan <warfear@hotmail.com>
2018-09-08 23:40:08 +0800

    Edited SortCommand usage message

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -9,7 +9,7 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Sorts all persons in the address book alphabetically.\n"
             + "Example: " + COMMAND_WORD;
 
 
