f415874d530167dd1b2426f23c535d8e3bcf8465
Zong Sien <e0200944@u.nus.edu>
2018-09-08 16:13:54 +0800

    addressbook/commands/HelpCommand: Add help support for star command

--- src/seedu/addressbook/commands/StarCommand.java
+++ src/seedu/addressbook/commands/StarCommand.java
@@ -3,14 +3,16 @@ package seedu.addressbook.commands;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList;
 
 import java.util.*;
 
 public class StarCommand extends Command {
     public static final String COMMAND_WORD = "star";
-    public static final String MESSAGE_USAGE = COMMAND_WORD;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the selected person with a star. " +
+            "The name (case-sensitive) should be the full name.\n" +
+            "Parameter: NAME\n" +
+            "Example: " + COMMAND_WORD + " John Doe";
     public static final String MESSAGE_SUCCESS = "New person starred: %1$s";
     public static final String MESSAGE_NAME_INVALID = "Name is invalid.";
     public static final String MESSAGE_PERSON_NOT_FOUND = "Unable to find %1$s";
