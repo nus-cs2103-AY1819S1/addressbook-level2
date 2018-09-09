c58442bbcfc6bef8aef84503e6c3e75bf84dfda9
风之倒影 <1229983126@qq.com>
2018-09-09 15:04:29 +0800

    Junit test of new find command added

--- test/java/seedu/addressbook/commands/FindCommandTest.java
+++ test/java/seedu/addressbook/commands/FindCommandTest.java
@@ -22,24 +22,44 @@ public class FindCommandTest {
 
     @Test
     public void execute() throws IllegalValueException {
-        //same word, same case: matched
+        //same word, same case of fields: matched
         assertFindCommandBehavior(new String[]{"Amy"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior(new String[]{"Clementi"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior(new String[]{"91119111"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior(new String[]{"cd@gmail.com"}, Arrays.asList(td.candy));
+        assertFindCommandBehavior(new String[]{"test"}, Arrays.asList(td.dan));
 
-        //same word, different case: not matched
+        //same word, different case of fields: not matched
         assertFindCommandBehavior(new String[]{"aMy"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"ClementI"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"cd@Gmail.com"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"Test"}, Collections.emptyList());
 
         //partial word: not matched
         assertFindCommandBehavior(new String[]{"my"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"Clemti"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"919111"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"cd@g.com"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"est"}, Collections.emptyList());
 
-        //multiple words: matched
+        //multiple words with different fields: matched
         assertFindCommandBehavior(new String[]{"Amy", "Bill", "Candy", "Destiny"},
                 Arrays.asList(td.amy, td.bill, td.candy));
+        assertFindCommandBehavior(new String[]{"Amy", "92229222", "cd@gmail.com", "test"},
+                Arrays.asList(td.amy, td.bill, td.candy, td.dan));
 
-        //repeated keywords: matched
-        assertFindCommandBehavior(new String[]{"Amy", "Amy"}, Arrays.asList(td.amy));
+        //multiple keywords of different fields for one person: matched
+        assertFindCommandBehavior(new String[]{"Amy", "91119111", "ab@gmail.com"}, Arrays.asList(td.amy));
 
-        //Keyword matching a word in address: not matched
-        assertFindCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
+        //repeated keywords for one person: matched
+        assertFindCommandBehavior(new String[]{"Amy", "91119111"}, Arrays.asList(td.amy));
+
+        //keyword matching a word which is private: not matched
+        assertFindCommandBehavior(new String[]{"93339333"}, Collections.emptyList());
+
+        //keyword matching with some words which are private: matched
+        assertFindCommandBehavior(new String[]{"Amy", "bc@gmail.com", "93339333", "test"},
+                Arrays.asList(td.amy, td.bill, td.dan));
     }
 
     /**
