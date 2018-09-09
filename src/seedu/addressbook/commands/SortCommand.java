b28a871344bad5b4a7ecf4ec5c5818beea0111da
DESKTOP-NSV5A3C\Lee Yu Cong <yu_cong96@hotmail.com>
2018-09-09 20:14:50 +0800

    SortCommand added

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,10 +1,37 @@
 package seedu.addressbook.commands;
 
-public class SortCommand extends Command{
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.UniquePersonList;
+
+import java.util.*;
+
+public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_SUCCESS = "Successfully sorted.";
+    public static final String MESSAGE_EMPTY_BOOK = "Nothing to sort.";
 
     @Override
     public CommandResult execute() {
-        return new CommandResult("");
+        PriorityQueue<Person> arrangedPersons = new PriorityQueue<>(new Comparator<Person>() {
+            public int compare(Person p1, Person p2) {
+                return p2.getName().fullName.compareTo(p1.getName().fullName);
+            }
+        });
+        try {
+            for (Person person : addressBook.getAllPersons()) {
+                arrangedPersons.add(person);
+            }
+            addressBook.clear();
+            while (!arrangedPersons.isEmpty()) {
+                addressBook.addPerson(arrangedPersons.poll());
+            }
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (NullPointerException err) {
+            return new CommandResult(MESSAGE_EMPTY_BOOK);
+        } catch (UniquePersonList.DuplicatePersonException dpe) {
+            // not possible to reach here
+            return new CommandResult("");
+        }
     }
 }
