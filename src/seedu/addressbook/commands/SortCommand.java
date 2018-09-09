8438eadcb53ff5eefd5d26c184cbd56da5bc2edf
DESKTOP-NSV5A3C\Lee Yu Cong <yu_cong96@hotmail.com>
2018-09-09 19:11:39 +0800

    AddressBook recognised sort

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.commands;
+
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult("");
+    }
+}
