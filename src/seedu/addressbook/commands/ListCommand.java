bfcfeddb81409d47d146730a13b6fe1dfb5e9ba5
denzelchung <denzelchung@hotmail.com>
2018-09-09 21:10:46 +0800

    no message

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -1,9 +1,7 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
-
 import java.util.List;
-
 import java.util.Comparator;
 
 
@@ -22,12 +20,6 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        allPersons.sort(new Comparator<ReadOnlyPerson>() {
-            @Override
-            public int compare(ReadOnlyPerson o1, ReadOnlyPerson o2) {
-                return o1.getName().fullName.compareTo(o2.getName().fullName);
-            }
-        });
 
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
