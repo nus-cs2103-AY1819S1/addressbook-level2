d22e51b0ba72f11c276a7f4a1ced687e54805e40
Rebecca <rebecca_tan_97@hotmail.com>
2018-09-09 18:29:11 +0800

    Add dummy findPhone command which does same thing as find

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -17,6 +17,7 @@ import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.DeleteCommand;
 import seedu.addressbook.commands.ExitCommand;
 import seedu.addressbook.commands.FindCommand;
+import seedu.addressbook.commands.FindPhoneCommand;
 import seedu.addressbook.commands.HelpCommand;
 import seedu.addressbook.commands.IncorrectCommand;
 import seedu.addressbook.commands.ListCommand;
@@ -83,6 +84,9 @@ public class Parser {
             return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
+            return prepareFind(arguments);
+
+        case FindPhoneCommand.COMMAND_WORD:
             return prepareFind(arguments);
 
         case ListCommand.COMMAND_WORD:
