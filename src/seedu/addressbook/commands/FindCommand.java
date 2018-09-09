b9427b778e24b10a7f1e824141a08d0200ba5932
Tian Xueyan <daisytianxueyan@gmail.com>
2018-09-09 13:14:41 +0800

    Update findCommand to enable case-insensitive find.

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -17,7 +17,7 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords (non-case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
@@ -42,6 +42,7 @@ public class FindCommand extends Command {
 
     /**
      * Retrieves all persons in the address book whose names contain some of the specified keywords.
+     * The person will be returned regardless of which case the keywords are entered.
      *
      * @param keywords for searching
      * @return list of persons found
@@ -50,7 +51,12 @@ public class FindCommand extends Command {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final Set<String> wordsInNameInTwoCases = new HashSet<>();
+            for (String word : wordsInName) {
+                wordsInNameInTwoCases.add(word.toUpperCase());
+                wordsInNameInTwoCases.add(word.toLowerCase());
+            }
+            if (!Collections.disjoint(wordsInNameInTwoCases, keywords)) {
                 matchedPersons.add(person);
             }
         }
