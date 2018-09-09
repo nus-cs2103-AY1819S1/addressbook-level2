410aa0507d290769a8d4e3d88b45313bbd81a41f
Ang Zhi Kai <ang-zhikai@outlook.com>
2018-09-09 17:34:02 +0800

    SortCommand.java: Update MESSAGE_USAGE
    Message usage of the command has been updated with an appropriate new line ending.

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -7,7 +7,8 @@ public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the address book by name "
+            + "in alphabetical order.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
