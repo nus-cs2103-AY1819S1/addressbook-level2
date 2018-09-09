be2bff9c2f04c4e3ebb430578e7db6e58b100918
marcus koh <kohchihao@gmail.com>
2018-09-09 11:33:19 +0800

    Add SortCommand.java and Paraser.java: Update parseCommand() method to include SortCommand

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.commands;
+
+public class SortCommand extends Command {
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers in alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+}
