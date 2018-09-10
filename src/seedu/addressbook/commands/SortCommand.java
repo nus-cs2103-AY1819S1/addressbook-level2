3899814f15b38c9144ab0d3e445e17d710b838a3
Jun Rong <jun_rong5@hotmail.com>
2018-09-10 12:45:28 +0800

    SortCommand.java: add message_usage for the sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,6 +6,9 @@ package seedu.addressbook.commands;
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sort all persons in the address book in lexical order.\n"
+            + "Example: " + COMMAND_WORD;
     public static final String MESSAGE_SUCCESS = "List sorted in lexical order.";
 
     @Override
