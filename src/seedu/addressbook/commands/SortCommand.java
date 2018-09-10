f7f2d249464cf6fac39d5a3f22662d6decfd1679
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-09-10 18:53:35 +0800

    Sort command displays success message correctly

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -8,6 +8,8 @@ public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
+    public static final String MESSAGE_SUCCESS = "Address Book sorted!";
+
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Sorts all persons in the address book by name by alphabetic order.\n"
             + "Example: " + COMMAND_WORD;
@@ -16,7 +18,6 @@ public class SortCommand extends Command {
     @Override
     public CommandResult execute() {
         addressBook.sort();
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
