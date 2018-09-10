87c6fb0ec79cc4c59738ad129fa33507b26f9f8b
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-09-10 19:00:20 +0800

    Write javadoc for sortCommand class

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,9 +1,8 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.data.person.ReadOnlyPerson;
-
-import java.util.List;
-
+/**
+ * Sorts the address book.
+ */
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
