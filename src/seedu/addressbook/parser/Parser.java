d9ae6bc15b7821438d5ed31297d3d9a93398a2dd
DESKTOP-DAUN7QD\Kenneth <kennethgoh96.989@gmail.com>
2018-09-10 22:38:08 +0800

    Incorporate ImportCommand acceptability in HelpCommand.java and Parser.java

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -19,6 +19,7 @@ import seedu.addressbook.commands.DeleteCommand;
 import seedu.addressbook.commands.ExitCommand;
 import seedu.addressbook.commands.FindCommand;
 import seedu.addressbook.commands.HelpCommand;
+import seedu.addressbook.commands.ImportCommand;
 import seedu.addressbook.commands.IncorrectCommand;
 import seedu.addressbook.commands.ListCommand;
 import seedu.addressbook.commands.ViewAllCommand;
@@ -88,7 +89,10 @@ public class Parser {
 
         case FindCommand.COMMAND_WORD:
             return prepareFind(arguments);
-
+            
+        case ImportCommand.COMMAND_WORD:
+            return new ImportCommand();
+            
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
 
