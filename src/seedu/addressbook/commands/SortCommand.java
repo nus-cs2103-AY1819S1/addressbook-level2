50ee5d2b3a3e30a8e3506b11bd90a9524f161528
Jun Rong <jun_rong5@hotmail.com>
2018-09-10 12:39:18 +0800

    SortCommand.java: update execute() method to sort address book and return success message

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,11 +1,16 @@
 package seedu.addressbook.commands;
 
+/**
+ * Sort the address book.
+ */
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_SUCCESS = "List sorted in lexical order.";
 
     @Override
     public CommandResult execute() {
-        return new CommandResult("Sort activated!");
+        addressBook.sortAllPersons();
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
