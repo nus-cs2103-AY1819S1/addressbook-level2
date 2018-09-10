a5cdd3ed5360145f82eafd7068f6f20cb6827dbc
LAPTOP-C1KL05RT\lionel <e0203420@u.nus.edu>
2018-09-10 14:31:44 +0800

    UtilsTest.java: Remove unused imports

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,12 +1,9 @@
 package seedu.addressbook.common;
 
-import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
