5ae927e529d9d6613e322fef6bb9ee19979c9818
jhengy <35480886+jhengy@users.noreply.github.com>
2018-09-10 16:57:25 +0800

    AddCommandTest.java: modified to take into account of additional field of the commandResult

--- test/java/seedu/addressbook/commands/AddCommandTest.java
+++ test/java/seedu/addressbook/commands/AddCommandTest.java
@@ -1,28 +1,14 @@
 package seedu.addressbook.commands;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.junit.Test;
-
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.data.person.*;
 import seedu.addressbook.util.TestUtil;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class AddCommandTest {
     private static final List<ReadOnlyPerson> EMPTY_PERSON_LIST = Collections.emptyList();
@@ -125,7 +111,8 @@ public class AddCommandTest {
 
         assertTrue(people.contains(p));
         assertEquals(1, people.immutableListView().size());
-        assertFalse(result.getRelevantPersons().isPresent());
+        //assertFalse(result.getRelevantPersons().isPresent());// result now have a component of relevant person
+        assertTrue(result.getRelevantPersons().isPresent());
         assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, p), result.feedbackToUser);
     }
 
