e782c2f2bdd679afeb2e7ddef61dfce81fe47f3c
Jin Ying <developer.tjy@gmail.com>
2018-09-10 13:14:13 +0800

    SortCommand.java: Set up class with attributes and methods

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,23 @@
 package seedu.addressbook.commands;
 
-public class SortCommand {
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+/**
+ * Lists all persons sorted in alphabetical order in the address book to the user.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons sorted in alphabetical order in the address book as a list with index numbers.\n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> sortedAllPersons = addressBook.getAllPersons().sortByAlphabetToImmutableList();
+        return new CommandResult(getMessageForPersonListShownSummary(sortedAllPersons), sortedAllPersons);
+    }
 }
