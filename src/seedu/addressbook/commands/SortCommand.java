05b70a1b4f1c2270a90d681fcfb09dcd8a6f432f
alex <alex_koh_@live.com>
2018-09-10 23:32:48 +0800

    reformat SortCommand and changed some comments

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,14 +6,15 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * Lists all persons in the address book to the user.
+ * Sorts and list everyone in the address book.
  */
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers sorted according to the the specified field.\n"
+            + ": Displays all persons in the address book as a list with index "
+            + "numbers sorted according to the the specified field.\n"
             + "Example: " + COMMAND_WORD + " name";
 
     private final String keyword;
@@ -46,19 +47,27 @@ public class SortCommand extends Command {
      * @return Comparator used to sort the result
      */
     private Comparator<ReadOnlyPerson> getComparator() {
-        switch(keyword) {
+        switch (keyword) {
             case "phone":
-                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getPhone().value.compareTo(y.getPhone().value);
+                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getPhone()
+                        .value
+                        .compareTo(y.getPhone().value);
 
             case "email":
-                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getEmail().value.compareTo(y.getEmail().value);
+                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getEmail()
+                        .value
+                        .compareTo(y.getEmail().value);
 
             case "address":
-                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getAddress().value.compareTo(y.getAddress().value);
+                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getAddress()
+                        .value
+                        .compareTo(y.getAddress().value);
 
             case "name":
             default:
-                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getName().fullName.compareTo(y.getName().fullName);
+                return (ReadOnlyPerson x, ReadOnlyPerson y) -> x.getName()
+                        .fullName
+                        .compareTo(y.getName().fullName);
         }
     }
 }
