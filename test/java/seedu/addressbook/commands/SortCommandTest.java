6481263965cf0fed62d4f681da795209339ac431
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-09-10 19:45:22 +0800

    Add getAddressBook() method in TypicalPersons.java

---
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.util.TypicalPersons;
+
+import static org.junit.Assert.*;
+
+public class SortCommandTest {
+
+
+    @Test
+    public void sortTest() {
+
+    }
+}
