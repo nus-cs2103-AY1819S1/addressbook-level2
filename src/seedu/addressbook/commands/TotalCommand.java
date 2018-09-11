08b52e33fe3f0572f8bd2d1bcd51fb17720af367
HEARTOFAL1ON <ngszeyao@gmail.com>
2018-09-12 06:25:17 +0800

    TotalCommand.java: fully implement class

--- src/seedu/addressbook/commands/TotalCommand.java
+++ src/seedu/addressbook/commands/TotalCommand.java
@@ -1,4 +1,20 @@
 package seedu.addressbook.commands;
 
-public class TotalCommand {
+public class TotalCommand extends Command {
+
+    public static final String COMMAND_WORD = "total";
+    public static final String MESSAGE_USAGE = "Returns the total number of entries in the address book.\n"
+            + "Example: " + COMMAND_WORD;
+    public static final String MESSAGE_SINGULAR = " entry found!";
+    public static final String MESSAGE_PLURAL = " entries found!";
+
+    @Override
+    public CommandResult execute() {
+        int size = addressBook.getSize();
+        if (size == 1) {
+            return new CommandResult(size + MESSAGE_SINGULAR);
+        } else {
+            return new CommandResult(size + MESSAGE_PLURAL);
+        }
+    }
 }
