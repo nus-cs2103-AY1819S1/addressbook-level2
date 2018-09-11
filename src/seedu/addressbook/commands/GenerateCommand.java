f88a3b73a260429030fbfc7f6b1a3c5d44fa0ce7
Ong Yu-He <yuhembp@Ongs-MacBook-Pro-2.local>
2018-09-12 01:02:52 +0800

    GenerateCommand.java: New file with  generate command template

---
+++ src/seedu/addressbook/commands/GenerateCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Generates 5 persons into the address book.
+ */
+public class GenerateCommand extends Command {
+
+    public static final String COMMAND_WORD = "generate";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Generates 5 persons into the address book.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
