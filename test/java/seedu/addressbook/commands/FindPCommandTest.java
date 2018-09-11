c6b8b0d7cd24cf06dbe51d6b13c5aeac43c25f65
darieca <dariecalim@hotmail.com>
2018-09-11 18:12:40 +0800

    [FindPCommandTest.java] add findP command test

--- test/java/seedu/addressbook/commands/FindPCommandTest.java
+++ test/java/seedu/addressbook/commands/FindPCommandTest.java
@@ -1,4 +1,55 @@
 package seedu.addressbook.commands;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TypicalPersons;
+
 public class FindPCommandTest {
+
+    private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+    private final TypicalPersons td = new TypicalPersons();
+
+    @Test
+    public void execute() throws IllegalValueException {
+        //same phone number: matched
+        assertFindPCommandBehavior("91119111", Arrays.asList(td.amy));
+
+        //different phone number: not matched
+        assertFindPCommandBehavior("91234567", Collections.emptyList());
+
+        //partially matched phone number: not matched
+        assertFindPCommandBehavior("9111", Collections.emptyList());
+
+        //matched phone number with extra digits : not matched
+        assertFindPCommandBehavior("9111911111", Collections.emptyList());
+    }
+
+    /**
+     * Executes the findP command for given phone number and verifies
+     * the result matches the persons in the expectedPersonList exactly.
+     */
+    private void assertFindPCommandBehavior(String phoneNo, List<ReadOnlyPerson> expectedPersonList) {
+        FindPCommand command = createFindPCommand(phoneNo);
+        CommandResult result = command.execute();
+
+        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
+    }
+
+    private FindPCommand createFindPCommand(String phoneNo) {
+        FindPCommand command = new FindPCommand(phoneNo);
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+
 }
