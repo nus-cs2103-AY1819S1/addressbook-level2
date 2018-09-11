389282de65b7ad97d9fc3021f444b1ea4bd3489d
Ong Yu-He <yuhembp@Ongs-MacBook-Pro-2.local>
2018-09-12 03:04:13 +0800

    GenerateCommand.java: created dummy person for testing command

--- src/seedu/addressbook/commands/GenerateCommand.java
+++ src/seedu/addressbook/commands/GenerateCommand.java
@@ -1,7 +1,9 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -13,13 +15,28 @@ public class GenerateCommand extends Command {
     public static final String COMMAND_WORD = "generate";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Generates 5 persons into the address book.\n"
+            + ": Generates 4 persons into the address book.\n"
             + "Example: " + COMMAND_WORD;
 
+    private Person createPeople() throws IllegalValueException {
+        Person person1 = new Person(
+                new Name("Logan Paul"),
+                new Phone("98881919", false),
+                new Email("jp@gmail.com", false),
+                new Address("Japan Forest", false),
+                Collections.emptySet());
+        return person1;
+    }
 
     @Override
     public CommandResult execute() {
-        //List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult("Successfully added 5 persons");
+        try {
+            Person p1 = createPeople();
+            addressBook.addPerson(p1);
+            return new CommandResult("Successfully added 5 persons");
+        } catch (IllegalValueException ive) {
+            return new CommandResult("Person names in list are  wrongly set.xx");
+        }
+
     }
 }
