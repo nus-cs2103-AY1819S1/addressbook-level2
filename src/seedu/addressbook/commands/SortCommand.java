11048151c5602ebc73c97b7d5d9fd123b4006ce2
florafong97 <e0042072@u.nus.edu>
2018-09-12 00:49:11 +0800

    Edit expected output

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -8,7 +8,7 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts all persons in address book. \n"
+            + ": Sorts all persons in address book.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SORTED = "Address book has been sorted";
