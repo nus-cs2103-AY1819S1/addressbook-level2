7c9f93f423fa99f3c74ba1d00716dbf80a3f7098
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-09-11 15:56:22 +0800

    Added exception handling to execute() in UpdateCommand and removed newline from DeleteCommand

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -17,14 +17,15 @@ public class UpdateCommand extends Command{
             + "Example: " + COMMAND_WORD +
             " 1 John Smith p/11111111 e/john@email.com a/21st Orchard Rd, #12-02 Garden Towers t/friends";
     public static final String MESSAGE_SUCCESS = "%1$s updated.";
+    public static final String MESSAGE_DUPLICATE_DETAILS = "Details provided are the same.";
     private final Person updatePerson;
-    private final int targetVisibleIndex;
 
     public UpdateCommand(int targetVisibleIndex, String name,
                          String phone, boolean isPhonePrivate,
                          String email, boolean isEmailPrivate,
                          String address, boolean isAddressPrivate,
                          Set<String> tags) throws IllegalValueException {
+        super(targetVisibleIndex);
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
@@ -36,12 +37,22 @@ public class UpdateCommand extends Command{
                 new Address(address, isAddressPrivate),
                 tagSet
         );
-        this.targetVisibleIndex = targetVisibleIndex;
     }
 
     @Override
     public CommandResult execute() {
-        return null;
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            addressBook.addPerson(updatePerson);
+            addressBook.removePerson(target);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, updatePerson));
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        } catch (UniquePersonList.DuplicatePersonException dpe) {
+            return new CommandResult(MESSAGE_DUPLICATE_DETAILS);
+        }
     }
 
 }
