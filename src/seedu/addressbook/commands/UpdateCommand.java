243b8661389716b01ee280cd0135da461d15bd1a
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-09-11 15:34:33 +0800

    Added constructor and execute method to UpdateCommand class

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -1,6 +1,15 @@
 package seedu.addressbook.commands;
 
-public class UpdateCommand {
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+import seedu.addressbook.data.tag.Tag;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UpdateCommand extends Command{
     public static final String COMMAND_WORD = "update";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates details of the person " +
             "identified by the index number used in the last person listing.\n"
@@ -8,4 +17,31 @@ public class UpdateCommand {
             + "Example: " + COMMAND_WORD +
             " 1 John Smith p/11111111 e/john@email.com a/21st Orchard Rd, #12-02 Garden Towers t/friends";
     public static final String MESSAGE_SUCCESS = "%1$s updated.";
+    private final Person updatePerson;
+    private final int targetVisibleIndex;
+
+    public UpdateCommand(int targetVisibleIndex, String name,
+                         String phone, boolean isPhonePrivate,
+                         String email, boolean isEmailPrivate,
+                         String address, boolean isAddressPrivate,
+                         Set<String> tags) throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.updatePerson = new Person(
+                new Name(name),
+                new Phone(phone, isPhonePrivate),
+                new Email(email, isEmailPrivate),
+                new Address(address, isAddressPrivate),
+                tagSet
+        );
+        this.targetVisibleIndex = targetVisibleIndex;
+    }
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+
 }
