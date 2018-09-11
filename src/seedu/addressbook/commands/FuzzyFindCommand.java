7356e6dee6b629ba349e1b81d189826397ba13c7
hrxiong <e0367695@u.nus.edu>
2018-09-11 19:34:31 +0800

    update class FuzzyFindCommand to have the same behavior as FindCommand as well as tests

--- src/seedu/addressbook/commands/FuzzyFindCommand.java
+++ src/seedu/addressbook/commands/FuzzyFindCommand.java
@@ -1,5 +1,13 @@
 package seedu.addressbook.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
 /**
  * Finds and lists all persons in address book whose name is similar with any of the argument keywords.
  * "similar" means that levenshtein distance of two words is less than or equal to 2.
@@ -8,12 +16,44 @@ public class FuzzyFindCommand extends Command {
 
     public static final String COMMAND_WORD = "fuzzyFind";
 
-    public FuzzyFindCommand() {
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final Set<String> keywords;
+
+    public FuzzyFindCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * Returns a copy of keywords in this command.
+     */
+    public Set<String> getKeywords() {
+        return new HashSet<>(keywords);
     }
 
     @Override
     public CommandResult execute() {
-        return new CommandResult("");
+        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
     }
 
+    /**
+     * Retrieves all persons in the address book whose names contain some of the specified keywords.
+     *
+     * @param keywords for searching
+     * @return list of persons found
+     */
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
+        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : addressBook.getAllPersons()) {
+            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
+            if (!Collections.disjoint(wordsInName, keywords)) {
+                matchedPersons.add(person);
+            }
+        }
+        return matchedPersons;
+    }
 }
--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -253,6 +253,15 @@ public class Parser {
     }
 
     private Command prepareFuzzyFind(String args) {
-        return new FuzzyFindCommand();
+        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.MESSAGE_USAGE));
+        }
+
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
+        return new FuzzyFindCommand(keywordSet);
     }
 }
