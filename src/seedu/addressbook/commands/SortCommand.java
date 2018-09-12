6c854e1f9e015f2fcc566d4f8383cfc43da91e1b
Ju <jluuiss@gmail.com>
2018-09-12 20:11:20 +0800

    Add Sort command to UI, add action to command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,4 +6,12 @@ public class SortCommand extends Command{
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + "Sorts the list of users by name.\n"
             + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Address Book sorted.\n"
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sortAddressBook();
+        return CommandResult(MESSAGE_SUCCESS);
+    }
 }
--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -15,6 +15,7 @@ import seedu.addressbook.commands.AddCommand;
 import seedu.addressbook.commands.ClearCommand;
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.DeleteCommand;
+import seedu.addressbook.commands.SortCommand;
 import seedu.addressbook.commands.ExitCommand;
 import seedu.addressbook.commands.FindCommand;
 import seedu.addressbook.commands.HelpCommand;
@@ -87,6 +88,9 @@ public class Parser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case SortCommand.COMMAND_WORD:
+            return new SortCommand();
 
         case ViewCommand.COMMAND_WORD:
             return prepareView(arguments);
