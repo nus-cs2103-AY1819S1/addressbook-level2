0a73288872e83374ef5ecb5d2d850ecb6f852377
vig42 <vighnesh.iyer@u.nus.edu>
2018-09-12 21:58:52 +0800

    Add unit tests for Utils#isAnyNull(Object...) method

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -2,6 +2,7 @@ package seedu.addressbook.common;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assert
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +35,27 @@ public class UtilsTest {
         assertNotUnique(null, 1, Integer.valueOf(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+    }
+
+    @Test
+    public void isAnyNull(Object... items) {
+        // only 1 object
+        assertTrue(Utils.isAnyNull((Object) null));
+        assertFalse(Utils.isAnyNull(1);
+        assertFalse(Utils.isAnyNull("");
+        assertFalse(Utils.isAnyNull("something");
+        assertFalse(Utils.isAnyNull(new char[]);
+        assertFalse(Utils.isAnyNull(true);
+
+        // multiple objects of same type
+        assertTrue(Utils.isAnyNull(null, null, null));
+        assertFalse(Utils.isAnyNull(1,2,3));
+        assertFalse(Utils.isAnyNull("", "Hello", "test"));
+        assertFalse(Utils.isAnyNull(true, false));
+
+        // multiple objects of different type
+        assertTrue(Utils.isAnyNull(1, 2, 3, "Hello", null, "test", new int[], true));
+        assertFalse(Utils.isAnyNull(1, 2, 3, "Hello", "test", new int[], false));
     }
 
     private void assertAreUnique(Object... objects) {
