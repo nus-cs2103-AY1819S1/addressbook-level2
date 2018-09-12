f3dad56832616dce32621a60f49bc05a0b7c1793
DESKTOP-L47CTGG\zlz <zhanglizhi.relax@gmail.com>
2018-09-12 14:09:47 +0800

    updaete user guide

--- src/seedu/addressbook/commands/NewestEntryCommand.java
+++ src/seedu/addressbook/commands/NewestEntryCommand.java
@@ -1,7 +1,6 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
-import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 import java.util.ArrayList;
