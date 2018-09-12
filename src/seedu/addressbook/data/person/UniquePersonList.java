5ad384430f896d188637193265fba67f17abcac4
vig42 <vighnesh.iyer@u.nus.edu>
2018-09-12 23:53:59 +0800

    data/person/UniquePersonList: Import classes individually

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,10 +1,16 @@
 package seedu.addressbook.data.person;
 
-import java.util.*;
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
 
 /**
