cad5ad46b92932c759e9752ac74b53251646ccff
Rezky Arizaputra <rezkyarizaputra@yahoo.co.id>
2018-09-12 22:05:12 +0800

    Create CompareName.java

---
+++ src/seedu/addressbook/data/person/CompareName.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+ class CompareName implements Comparator<Person>{
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().toString()
+                .compareTo(p2.getName().toString());
+    }
+}
