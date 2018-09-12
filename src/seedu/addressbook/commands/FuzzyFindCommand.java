25450da5e711f08b1e0cfafb6fa3980d920702c2
hrxiong <e0367695@u.nus.edu>
2018-09-12 12:33:31 +0800

    Update the comments

--- src/seedu/addressbook/commands/FuzzyFindCommand.java
+++ src/seedu/addressbook/commands/FuzzyFindCommand.java
@@ -16,7 +16,7 @@ public class FuzzyFindCommand extends Command {
 
     public static final String COMMAND_WORD = "fuzzyFind";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names are similar to any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
@@ -41,7 +41,7 @@ public class FuzzyFindCommand extends Command {
     }
 
     /**
-     * Retrieves all persons in the address book whose names contain some of the specified keywords.
+     * Retrieves all persons in the address book whose names are similar to some of the specified keywords.
      *
      * @param keywords for searching
      * @return list of persons found
@@ -50,7 +50,10 @@ public class FuzzyFindCommand extends Command {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         final int THRESHOLD = 2;
 
-
+        /*
+        go through all combination between keywords and wordsInName
+        if a person was added into the return list, break from the two inner loops immediately
+        */
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             boolean hasThisOne = false;
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
@@ -59,12 +62,10 @@ public class FuzzyFindCommand extends Command {
                     if (isLevenshteinDistanceLessThanOrEqualToThreshold(wordInName, keyword, THRESHOLD)) {
                         matchedPersons.add(person);
                         hasThisOne = true;
-                    }
-                    if(hasThisOne) {
                         break;
                     }
                 }
-                if (hasThisOne){
+                if (hasThisOne) {
                     break;
                 }
             }
@@ -72,6 +73,14 @@ public class FuzzyFindCommand extends Command {
         return matchedPersons;
     }
 
+    /**
+     * Judges if the levenshtein distance between wordInName and keyword is less than or equal to threshold.
+     *
+     * @param wordInName
+     * @param keyword
+     * @param Threshold the number that levenshtein distance need to be less than or equal to
+     * @return Returns true if levenshtein distance is less than or equal to threshold
+     */
     private boolean isLevenshteinDistanceLessThanOrEqualToThreshold(String wordInName, String keyword, int Threshold) {
         if (Math.abs(wordInName.length() - keyword.length()) > Threshold) {
             return false;
@@ -85,10 +94,17 @@ public class FuzzyFindCommand extends Command {
         return (matrix[wordInName.length()][keyword.length()] <= Threshold);
     }
 
-    private int setLevenshteinDistance(int[][] matrix, String wordInName, String keyword) {
-
-        int lengthX = keyword.length();
-        int lengthY = wordInName.length();
+    /**
+     * Gets the levenshtein distance between two strings using dynamic programming.
+     *
+     * @param matrix two-dimensional array to store the values during dynamic computing
+     * @param s1 string 1
+     * @param s2 string 2
+     * @return levenshtain distance between string 1 and string 2
+     */
+    private int setLevenshteinDistance(int[][] matrix, String s1, String s2) {
+        int lengthX = s2.length();
+        int lengthY = s1.length();
         //initialize matrix
         for (int i = 1; i <= lengthX; i++) {
             matrix[0][i] = i;
@@ -100,9 +116,9 @@ public class FuzzyFindCommand extends Command {
         //Dynamic Programming
         for (int j = 1; j <= lengthY; j++) {
             for (int i = 1; i <= lengthX; i++) {
-                int temp = wordInName.charAt(j - 1) == keyword.charAt(i - 1) ? 0 : 1;
+                int temp = s1.charAt(j - 1) == s2.charAt(i - 1) ? 0 : 1;
                 matrix[j][i] = Math.min(matrix[j - 1][i - 1] + temp,
-                                        Math.min(matrix[j][i - 1] + 1, matrix[j - 1][i] + 1));
+                        Math.min(matrix[j][i - 1] + 1, matrix[j - 1][i] + 1));
             }
         }
         return matrix[lengthY][lengthX];
