8e2d17d57ffaa206100ca2b9eed098d285c61654
Liau Jian Jie <liaujianjie@gmail.com>
2018-09-13 02:18:03 +0800

    Add sort method to UniquePersonList

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,11 +1,6 @@
 package seedu.addressbook.data.person;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
@@ -127,6 +122,13 @@ public class UniquePersonList implements Iterable<Person> {
      */
     public void clear() {
         internalList.clear();
+    }
+
+    /**
+     * Sorts the contacts in the address book by name in ascending alphabetical order.
+     */
+    public void sort() {
+        internalList.sort(Comparator.comparing(Person::getName));
     }
 
     @Override
