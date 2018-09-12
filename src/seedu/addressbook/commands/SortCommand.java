9d6c5e4334aee277257c02bec4306ab16f11fa78
Hilda-Ang <E0196709@u.nus.edu>
2018-09-12 19:27:00 +0800

    Add parser support for "sort" command and create SortCommand.java

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Shows all persons in the address book sorted in alphabetical order to the user.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book in an alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
