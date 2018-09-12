a224b3e0484ac338fc156bcf1dc0178964e332e5
Leslie Tang <aspiringdevslog@gmail.com>
2018-09-12 15:47:46 +0800

    CreateCommand.java: function takes in and return username and password Parser.java: add new input format for creating user

--- src/seedu/addressbook/commands/CreateCommand.java
+++ src/seedu/addressbook/commands/CreateCommand.java
@@ -11,43 +11,13 @@ public class CreateCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a new user account. "
             + "Parameters: USERNAME [p]p/PASSWORD \n"
             + "Example: " + COMMAND_WORD
-            + " user123 p/example password ";
+            + " user123 p/password123 ";
 
-    public static final String MESSAGE_SUCCESS = "New user created: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This user already exists.";
-
-//    private final Person toAdd;
-//
-//    /**
-//     * Convenience constructor using raw values.
-//     *
-//     * @throws IllegalValueException if any of the raw values are invalid
-//     */
-//    public CreateCommand(String name,
-//                      String phone, boolean isPhonePrivate,
-//                      String email, boolean isEmailPrivate,
-//                      String address, boolean isAddressPrivate,
-//                      Set<String> tags) throws IllegalValueException {
-//        final Set<Tag> tagSet = new HashSet<>();
-//        for (String tagName : tags) {
-//            tagSet.add(new Tag(tagName));
-//        }
-//        this.toAdd = new Person(
-//                new Name(name),
-//                new Phone(phone, isPhonePrivate),
-//                new Email(email, isEmailPrivate),
-//                new Address(address, isAddressPrivate),
-//                tagSet
-//        );
-//    }
-//
-    public CreateCommand() {
+    public CreateCommand(String username, String password) {
         System.out.println("Manage to call \"create\" command");
+        System.out.println("New User Created: Username: " + username + " Password: " + password);
     }
-//
-//    public ReadOnlyPerson getPerson() {
-//        return toAdd;
-//    }
+
     public static final String MESSAGE_ACCOUNT_CREATION_SUCCESSFUL = "Successfully created account!"; // Temporary message for testing
 
     @Override
