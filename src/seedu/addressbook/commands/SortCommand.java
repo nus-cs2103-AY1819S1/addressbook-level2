1527f7bb9721449d47fac022a3dcd2b0174017bc
Daniel Khoo <danielkhoo94@hotmail.com>
2018-09-12 11:36:03 +0800

    SortCommand: Update execute()

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -16,7 +16,7 @@ public class SortCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().sortImmutableListView();
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }
