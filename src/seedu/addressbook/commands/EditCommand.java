cc0f1b6ecbd64383a473af54c50d4610404736a9
Laptop-Josh Jr <j_.roberts@outlook.com>
2018-09-13 16:16:16 +0800

    Added Edit Person command

---
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -0,0 +1,69 @@
+package seedu.addressbook.commands;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.data.tag.Tag;
+
+/**
+ * Edits a person in the address book.
+ */
+public class EditCommand extends Command {
+  public static final String COMMAND_WORD = "edit";
+
+  public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits an existing person in the address book. "
+          + "Contact details can be marked private by prepending 'p' to the prefix.\n"
+          + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
+          + "Example: " + COMMAND_WORD
+          + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+
+  public static final String MESSAGE_SUCCESS = "Person edited: %1$s";
+
+  private final Person toEdit;
+
+  /**
+   * Convenience constructor using raw values.
+   *
+   * @throws IllegalValueException if any of the raw values are invalid
+   */
+  public EditCommand(String name,
+                    String phone, boolean isPhonePrivate,
+                    String email, boolean isEmailPrivate,
+                    String address, boolean isAddressPrivate,
+                    Set<String> tags) throws IllegalValueException {
+    final Set<Tag> tagSet = new HashSet<>();
+    for (String tagName : tags) {
+      tagSet.add(new Tag(tagName));
+    }
+    this.toEdit = new Person(
+            new Name(name),
+            new Phone(phone, isPhonePrivate),
+            new Email(email, isEmailPrivate),
+            new Address(address, isAddressPrivate),
+            tagSet
+    );
+  }
+
+  public EditCommand(Person toEdit) {
+    this.toEdit = toEdit;
+  }
+
+  public ReadOnlyPerson getPerson() {
+    return toEdit;
+  }
+
+  @Override
+  public CommandResult execute() {
+      addressBook.editPerson(toEdit);
+      return new CommandResult(String.format(MESSAGE_SUCCESS, toEdit));
+  }
+
+}
