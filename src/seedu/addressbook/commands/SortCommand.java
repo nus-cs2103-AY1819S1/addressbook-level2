1dfd644e42d0e9f6cd866880b7a65628c219dab4
Robert Victoreen <robert.victoreen@u.nus.edu>
2018-09-13 20:33:25 +0800

    Usage message format

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,7 +6,7 @@ package seedu.addressbook.commands;
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
-    public static final String MESSAGE_USAGE = "Sorts the address book in lexical order.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the address book in lexical order.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
