472c06d8032966f2d3eb7b24125e5404da9c8fbe
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-09-13 19:55:07 +0800

    Add getMessageUsage() method to Command and convert AddCommand as a sample.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -27,6 +27,25 @@ public class Command {
     }
 
     /**
+     * Generates the MESSAGE_USAGE for a command.
+     * @param commandWord The command's COMMAND_WORD
+     * @param commandDesc A brief description of what the command does
+     * @param commandParams The list of parameters the command accepts, or "none" if the command has no parameters
+     * @param commandEgParams An example of a set of valid parameter values to pass to the command, give "" if the command has no parameters
+     * @return the MESSAGE_USAGE for a command.
+     */
+    protected static String getMessageUsage(
+        String commandWord,
+        String commandDesc,
+        String commandParams,
+        String commandEgParams) {
+
+        return commandWord + ": " + commandDesc + "\n"
+            + "Parameters: " + commandParams + "\n"
+            + "Example: " + commandWord + " " + commandEgParams + "\n";
+    }
+
+    /**
      * Constructs a feedback message to summarise an operation that displayed a listing of persons.
      *
      * @param personsDisplayed used to generate summary
